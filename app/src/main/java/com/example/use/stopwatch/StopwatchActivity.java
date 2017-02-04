4328190a9051cdcdc23c4af769b5e9927f86ed7b
java2019 <java_2019@mail.ru>
2017-02-04 14:37:30 +0300

    Step 4

--- app/src/main/java/com/example/use/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/use/stopwatch/StopwatchActivity.java
@@ -10,30 +10,37 @@ public class StopwatchActivity extends Activity {
 
     private int seconds = 0;
     private boolean running;
-    private boolean wasrunning;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
         runTimer();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
     }
 
     @Override
-    protected void onStop() {
+    protected void onPause() {
         super.onStop();
-        wasrunning = running;
+        wasRunning = running;
     }
 
     @Override
-    protected void onStart() {
+    protected void onResume() {
         super.onStart();
-        if (wasrunning)
+        if (wasRunning)
         running = true;
     }
 
